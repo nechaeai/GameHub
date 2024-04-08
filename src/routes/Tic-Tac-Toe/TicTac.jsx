f21c2cc750c5c621f144c2d331fd9b4852ee6c7a
nechaeai <nechaeai@mail.uc.edu>
2024-04-07 22:48:28 -0400

    Update TicTac.jsx

--- src/routes/Tic-Tac-Toe/TicTac.jsx
+++ src/routes/Tic-Tac-Toe/TicTac.jsx
@@ -11,7 +11,6 @@ function Square({ value, onSquareClick }) {
 
 function Board({ xIsNext, squares, onPlay }) {
   function handleClick(i) {
-
     if (calculateWinner(squares) || squares[i]) {
       return;
     }
@@ -55,21 +54,19 @@ function Board({ xIsNext, squares, onPlay }) {
 }
 
 export default function Game() {
-  const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
   const currentSquares = history[currentMove];
 
   function handlePlay(nextSquares) {
     const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
     setHistory(nextHistory);
     setCurrentMove(nextHistory.length - 1);
-    setXIsNext(!xIsNext);
   }
 
   function jumpTo(nextMove) {
     setCurrentMove(nextMove);
-    setXIsNext(nextMove % 2 === 0);
   }
 
   const moves = history.map((squares, move) => {
