bb04737cdbb1babd6f194c636635836aa593839f
nechaeai <nechaeai@mail.uc.edu>
2024-04-12 17:23:05 -0400

    still adding images and structure

--- src/routes/Memory-Game/Memory.jsx
+++ src/routes/Memory-Game/Memory.jsx
@@ -1,36 +1,55 @@
 import React from 'react';
 import { useState } from 'react';
+import './Memory.css';
 
 const cardImages = [
-    { src: '/imgs/bottle.png'},
-    { src: '/imgs/diamond-ring.png'},
-    { src: '/imgs/samurai.png'},
-    { src: '/imgs/scroll.png'},
-    { src: '/imgs/shield.png'},
-    { src: '/imgs/sword.png'}
+    { src: './imgs/bottle.png' },
+    { src: './imgs/diamond-ring.png' },
+    { src: './imgs/samurai.png' },
+    { src: './imgs/scroll.png' },
+    { src: './imgs/shield.png' },
+    { src: './imgs/sword.png' }
 
 ];
 
 const Memory = () => {
-    
-const [cards, setCards] = useState([]);
-const [turns, setTurns] = useState(0);
+
+    const [cards, setCards] = useState([]);
+    const [turns, setTurns] = useState(0);
 
     const shuffleCards = () => {
         const shuffledCards = [...cardImages, ...cardImages]
-        .sort(() => Math.random() - 0.5)
-        .map((card) => ({ ...card, id: Math.random() }));
+            .sort(() => Math.random() - 0.5)
+            .map((card) => ({ ...card, id: Math.random() }));
 
-     setCards(shuffledCards);
-     setTurns(0);
+        setCards(shuffledCards);
+        setTurns(0);
     };
-console.log(cards, turns);
+    console.log(cards, turns);
     return (
         <div className='MemoryApp'>
             <h1>Magic Match</h1>
             <button onClick={shuffleCards}>Start Game</button>
+
+
+            <div className='card-grid'>
+                {cards.map((card) => (
+                    <div className='card' key={card.id}>
+                        <div>
+                        <img className='front' src={card.src} alt='card front' />
+                        <img className='back' src='./imgs/cover.png' alt='card back' />
+
+                    </div>
+
+
+
+        
         </div>
+        ))}
+        </div>
+        </div >
+    
     );
-};
+}
 
 export default Memory;
