9d30268a2d8c84df8f0ce4fc6f09debebc782d8d
BearcatTanner <Tannerdpatrick06@gmail.com>
2024-04-14 20:30:52 -0400

    Updated about page

--- src/routes/AboutPage.jsx
+++ src/routes/AboutPage.jsx
@@ -2,7 +2,8 @@ export default function AboutPage() {
     return (
       <div>
         <h1>About Page</h1>
-        <p>This site is a collection of my react-built games</p>
+        <p>This site is a collection of react-built games</p>
+        <p>It was built by Aleksei Nechaev, Raymond Ha, and Tanner Patrick</p>
       </div>
     );
   }
